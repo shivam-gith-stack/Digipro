8d027f64c253c9d4fdba44f822fc318c8b093918
shivamGupta31101408 <sg140890600@gmail.com>
2025-09-25 20:07:52 +0530

    changes immediates

--- backend/server.js
+++ backend/server.js
@@ -10,6 +10,7 @@ app.use(cors({
     origin: ["https://lumivxgrowth-three.vercel.app"], 
     credentials: true
 }));
+app.options("*", cors());
 
 app.use(express.json());
 app.use(bodyparser.json()); 
